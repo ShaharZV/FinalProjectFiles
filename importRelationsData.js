204e93f10186d7bebec9766981bdac6484dd8c8b
ShaharZV <93775537+ShaharZV@users.noreply.github.com>
2022-07-12 19:22:36 +0300

    Add files via upload

---
+++ importRelationsData.js
@@ -0,0 +1,8 @@
+//IMPORT STATES NAMES
+import relations from "./relationship_count.json" assert {type: "json"};
+
+window.importRelationsData2 = function importRelationsData1(){
+    return relations;
+};
+
+
